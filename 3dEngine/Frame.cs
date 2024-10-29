9de24445768059684ee79e4fa06a2705501c7a28
bezli <i@bezlikiy.ru>
2024-10-29 11:26:42 +0300

    memory fix2 (first fix is not full)

--- 3dEngine/Frame.cs
+++ 3dEngine/Frame.cs
@@ -5,13 +5,19 @@ public class Frame
     readonly Screen _screen = new Screen();
     public Vector3 ro = new Vector3(-2f, 0, 0);
     public Vector3 rd = new Vector3(0);
-    private readonly Shape _sphere = new Sphere(Vector3.Zero, Vector3.Zero);
-    private readonly LightBase _light = new LightBase(new Vector3());
+    private readonly Shape _sphere = new Sphere(new Vector3(0), Vector3.Zero);
+    private readonly LightBase _light = new LightBase(new Vector3(-2f,-0f,0f));
+
+    //private DateTime _lastFrame = DateTime.UtcNow;
+    //private double fps = 0;
     
     public void MainLoop()
     {
         for (int t = 0; true; t++)
         {
+            //_lastFrame = DateTime.UtcNow;
+            _light.Position.X = (float)Math.Sin(t * 0.001f);
+            _light.Position.Z = (float)Math.Cos(t * 0.001f);
             for (int j = 0; j < _screen.GetHeight(); j++)
             {
                 for (int i = 0; i < _screen.GetWidth(); i++)
@@ -23,15 +29,15 @@ public class Frame
                     rd.Y = uv.Y;
                     rd.Z = uv.X;
                     
-                    _light.Position.X = (float)Math.Sin(t * 0.001f) * 0.5f;
-                    _light.Position.Z = (float)Math.Cos(t * 0.001f) * 0.5f;
-                    
                     Vector2 intersection = _sphere.Intersection(ro, rd);
                     Vector3 itPoint = ro + rd * intersection.X;
                     int brightness = intersection.X > 0? _light.PointBright(itPoint) : 0;
+                    //_screen.Paint(ro.X + "  " + ro.Y, Vector2Int.Zero);
                     _screen.Paint(brightness);
                 }
             }
+            //fps = (DateTime.UtcNow - _lastFrame).TotalMicroseconds > 0? 1 / (DateTime.UtcNow - _lastFrame).TotalSeconds : 0;
+            //_screen.Paint( Double.Round(fps, 1).ToString(CultureInfo.InvariantCulture), Vector2Int.Zero);
         }
     }
 }
--- 3dEngine/Structure/Vector2Int.cs
+++ 3dEngine/Structure/Vector2Int.cs
@@ -9,28 +9,25 @@ public class Vector2Int(int x = 0, int y = 0)
     public Vector2Int(int a) : this(a, a) { }
     public Vector2Int() : this(0, 0) { }
     
-    private static Vector2Int _returnVectorInt = new Vector2Int();
-    private static Vector2 _returnVector = new Vector2();
-    
     public static Vector2Int operator +(Vector2Int a, Vector2Int b)
-    { _returnVectorInt.X = a.X + b.X; _returnVectorInt.Y = a.Y + b.Y; return _returnVectorInt; }
+    {  return new Vector2Int(a.X + b.X, a.Y + b.Y); }
     public static Vector2Int operator +(Vector2Int a, int b)
-    { _returnVectorInt.X = a.X + b; _returnVectorInt.Y = a.Y + b; return _returnVectorInt; }
+    {  return new Vector2Int(a.X + b, a.Y + b); }
     
     public static Vector2Int operator -(Vector2Int a, Vector2Int b)
-    { _returnVectorInt.X = a.X - b.X; _returnVectorInt.Y = a.Y - b.Y; return _returnVectorInt; }
+    {  return new Vector2Int(a.X - b.X, a.Y - b.Y); }
     public static Vector2Int operator -(Vector2Int a, int b)
-    { _returnVectorInt.X = a.X - b; _returnVectorInt.Y = a.Y - b; return _returnVectorInt; }
+    {  return new Vector2Int(a.X - b, a.Y - b); }
     
-    public static Vector2Int operator *(Vector2Int a, Vector2Int b)
-    { _returnVectorInt.X = a.X * b.X; _returnVectorInt.Y = a.Y * b.Y; return _returnVectorInt; }
+    public static float operator *(Vector2Int a, Vector2Int b)
+    { return a.X * b.X + a.Y * b.Y; }
     public static Vector2Int operator *(Vector2Int a, int b)
-    { _returnVectorInt.X = a.X * b; _returnVectorInt.Y = a.Y * b; return _returnVectorInt; }
+    {  return new Vector2Int(a.X * b, a.Y * b); }
     
     public static Vector2 operator /(Vector2Int a, Vector2Int b)
-    { _returnVector.X = (float)a.X / b.X; _returnVector.Y = (float)a.Y / b.Y; return _returnVector; }
+    {  return new Vector2((float)a.X / b.X, (float)a.Y / b.Y); }
     public static Vector2 operator /(Vector2Int a, Vector2 b)
-    { _returnVector.X = a.X / b.X; _returnVector.Y = a.Y / b.Y; return _returnVector; }
+    {  return new Vector2(a.X / b.X, a.Y / b.Y); }
     public static Vector2 operator /(Vector2Int a, int b)
-    { _returnVector.X = (float)a.X / b; _returnVector.Y = (float)a.Y / b; return _returnVector; }
+    {  return new Vector2((float)a.X / b, (float)a.Y / b); }
 }
